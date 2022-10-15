a2b925fbfd48ac69681fa664bf96d0b97342f4c3
Nastya Filippova <nastya.filippova87@gmail.com>
2022-10-16 00:33:31 +0300

    2 finished  tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+  if (str === null) {
+    str = 'null';
+  } else {
+    str = (str[Symbol.toPrimitive]) ? str : str.toString();
+  }
+  
+  let addition;
+ 
+  if (options.addition === null) {
+    addition = 'null';
+  } else if (options.addition === undefined){
+  } else if (options.addition[Symbol.toPrimitive]) {
+    addition = options.addition;
+  } else {
+    addition = (options.hasOwnProperty('addition')) ? (options.addition).toString() : '';
+  }
+    let optSep = (options.hasOwnProperty('additionSeparator')) ? options.additionSeparator : '|';
+  let add = '';
+  if ( options.hasOwnProperty('addition') && options.additionRepeatTimes > 0) {
+    
+    while (options.additionRepeatTimes > 1) {
+      add = add + addition + optSep;
+      options.additionRepeatTimes -= 1
+    }
+    add = add + addition;
+  }
+  if (options.hasOwnProperty('addition') && !options.hasOwnProperty('additionRepeatTimes')) {
+    add = addition;
+  }
+  let sep = (options.hasOwnProperty('separator')) ? options.separator : '+';
+  
+  let result = (str + add + sep).repeat(options.repeatTimes - 1) + str + add;
+    
+  return result
 }
 
 module.exports = {
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  'array': [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value='') {
+    this.array.push(String(value));
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if ((typeof position !== 'number')|| position < 1 || position > (this.getLength() - 1)) {
+      this.array = [];
+      throw new Error("You can\'t remove incorrect link!");
+    }
+    else {
+      this.array.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array = this.array.reverse();
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.forEach((elem, index) => this.array[index] = '( ' + elem + ' )');
+    let chain = this.array.join('~~');
+    this.array = [];
+    return chain
   }
 };
 
