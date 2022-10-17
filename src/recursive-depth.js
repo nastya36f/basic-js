76297e1297e31acb0c6ebe04a787d3aa38a928fd
Nastya Filippova <nastya.filippova87@gmail.com>
2022-10-17 17:48:45 +0300

    solved 2 tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,11 +12,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class DepthCalculator {
+
+
+  calculateDepth(arr) {
+    if (arr.findIndex(item => Array.isArray(item)) === -1) {
+      return 1;
+    } else {
+      return this.calculateDepth(arr.flat()) + 1    
+    }
   }
+
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getSeason(date) {
+  if (date === undefined)  {
+    return 'Unable to determine the time of year!'
+  } else if (date.hasOwnProperty('getMonth')) {
+    throw new Error("Invalid date!");
+  }
+  try {
+    let month = date.getMonth();
+    let result;
+    if (1 < month && month < 5) {
+      result = 'spring';
+    } else if (4 < month && month < 8) {
+      result = 'summer';
+    } else if (7 < month && month < 11) {
+      result = 'autumn|fall';
+    } else {
+      result = 'winter';
+    }
+    return result
+  } catch {
+    throw new Error("Invalid date!");
+  }
 }
+
 
 module.exports = {
   getSeason
